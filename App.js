76d6b52015a1ba312b813a8f972a99390f030b77
ariantomichael <mike.ak1412@gmail.com>
2017-06-21 01:50:27 +0900

    checkbox implemented successfully with delete task button, but still a bug on checkbox component where it checks an item after delete tasks button is pressed

--- App.js
+++ App.js
@@ -5,18 +5,17 @@ import CheckBox from 'react-native-checkbox';
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {elements: []};
+        this.state = {elements: [],isChecked:[]};
         this.itemCount = 0;
         this.clearText = this.clearText.bind(this);
     }
-
     clearText() {
         this._textInput.setNativeProps({text: ''});
     }
 
     render() {
         return (
-            <View style={{padding: 30, backgroundColor: "yellow"}}>
+            <View style={{padding: 30}}>
 
                 <TextInput
                     ref={component => this._textInput = component}
@@ -26,30 +25,43 @@ export default class App extends React.Component {
                     returnKeyLabel='done'
                     defaultValue=''
                     onSubmitEditing={(event) => {
-                        if(event.nativeEvent.text=="") return;
+                        if (event.nativeEvent.text == "") return;
                         this.state.elements.push({id: (this.itemCount++).toString(), text: event.nativeEvent.text});
                         this.setState({elements: this.state.elements});
                         this.clearText();
                     }}
                 />
                 {this.state.elements.map((elem) =>
-                    <Button title={elem.text} onPress={() => {
-                        this.state.elements.splice(this.state.elements.findIndex(el => el.id == elem.id), 1);
-                        this.setState({elements: this.state.elements});
-                    }
-                    } key={elem.id}>
-                    </Button>
+                    <CheckBox
+                        label={elem.text}
+                        onChange={(checked) => {
+                            if(!checked) {
+                                this.state.isChecked.push({id: elem.id, ischecked: !checked});
+                                this.setState({isChecked: this.state.isChecked});
+                            }else{
+                                this.state.isChecked.splice(this.state.isChecked.findIndex(i => i.id==elem.id),1);
+                                this.setState({isChecked: this.state.isChecked});
+                            }
+                        }}/>
+
                 )}
+                <Button title="delete checked task(s)" onPress={() => {
+                    this.state.isChecked.forEach( element => {
+                        this.state.elements.splice(this.state.elements.findIndex(el => el.id == element.id ),1);
+                    });
+                    this.setState({elements: this.state.elements, isChecked:[]});
+
+                }
+                } key='delete'>
+                </Button>
+
+                <Button title="console log" onPress={() => {
+                    console.log(this.state.isChecked);
+                    console.log(this.state.elements);
+                }
+                } key='log'>
+                </Button>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff000',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
